900510254e137295de0e74d7657c043c4c0c8492
michellejanosi <michelle.janosi@gmail.com>
2023-06-19 11:13:53 -0400

    Add functions

--- script.js
+++ script.js
@@ -3,12 +3,56 @@
 // Under Connection Requests, bonus: either button will decrease the Connection Requests number
 // Bonus: The accept button also increases the 'Your Connections' number.
 
-function editName() {
-  const profileName = document.querySelector(".profile-name");
-  let name = prompt("What is your name?")
-  profileName.innerHTML = name;
-}
+  const editProfileButton = document.querySelector(".edit-profile");
+  const acceptButtons = document.querySelectorAll(".accept");
+  const closeButtons = document.querySelectorAll(".close");
+  const requestsTitle = document.querySelector(
+    ".connection-requests .requests-title"
+  );
+  const yourConnectionsTitle = document.querySelector(
+    ".your-connections .requests-title"
+  );
 
-const editProfileBtn = document.querySelector(".edit-profile");
-editProfileBtn.addEventListener("click", editName);
+  // Function to edit profile name
+  editProfileButton.addEventListener("click", function () {
+    let newName = prompt("Please enter your new name:");
+    if (newName) {
+      document.querySelector(".profile-name").innerText = newName;
+    }
+  });
 
+  // Function to remove connection requests and adjust counters
+  acceptButtons.forEach(button => {
+    button.addEventListener("click", function () {
+      this.parentElement.parentElement.remove();
+      adjustRequestsCount(-1);
+      adjustConnectionsCount(1);
+    });
+  });
+
+  closeButtons.forEach(button => {
+    button.addEventListener("click", function () {
+      this.parentElement.parentElement.remove();
+      adjustRequestsCount(-1);
+    });
+  });
+
+  function adjustRequestsCount(amount) {
+    let currentCount = Number(
+      requestsTitle.querySelector(".gray-circle").innerText
+    );
+
+    let newCount = currentCount + amount;
+    requestsTitle.querySelector(".gray-circle").innerText = newCount.toString();
+  }
+
+  function adjustConnectionsCount(amount) {
+    let currentCount = Number(
+      yourConnectionsTitle.querySelector(".gray-circle").innerText
+    );
+    
+    let newCount = currentCount + amount;
+    yourConnectionsTitle.querySelector(
+      ".gray-circle"
+    ).innerText = newCount.toString();
+  }
